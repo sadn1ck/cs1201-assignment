66a1747e4b1243733ef4df3bfb50e52a23a03e73
Soham Pal <soham@DESKTOP-KTS5OIE.localdomain>
2020-06-17 00:20:17 +0530

    assignment 9 problem 1

---
+++ assignment9/Prob1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){ 
+    float a,b,x,y;
+    printf("Enter First Number: \nReal Part = ");
+    scanf("%f",&a);
+    printf("Imaginary Part = ");
+    scanf("%f",&b);
+    printf("Enter Second Number: \nReal Part = ");
+    scanf("%f",&x);
+    printf("Imaginary Part = ");
+    scanf("%f",&y);
+    int n;
+    printf("Operation:\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division \n");
+    scanf("%d",&n);
+    if(n==1) printf("%f + i%f", a+b, x+y);
+    else if(n==2) printf("%f + i%f", a-b, x-y);
+    else if(n==3) printf("%f + i%f", a*x - b*y, a*y + b*x);
+    else if(n==4) printf("%f + i%f", (a*x + b*y)/sqrt( x*x + y*y ), (b*x - a*y)/sqrt( x*x + y*y ));
+    printf("\n");
+    return 0;
+}
