b67a2709842dbe9e8f9d3803da363902e545cb43
Anik Das <anikdas0811@gmail.com>
2020-06-19 19:30:51 +0530

    Fixed variable declaration not done

--- assignment9/9_3_marks.c
+++ assignment9/9_3_marks.c
@@ -1,19 +1,19 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-struct record  
-{ 
+struct record
+{
     char name[20];
     int roll;
     int marks;
-}; 
+};
 
-int main () 
-{ 
-        FILE *infile; 
+int main ()
+{
+        FILE *infile;
         struct record input[10],temp;
-        
+        char path[100];
         printf("Enter file path to read (if it in the same directory enter the name) : ");
         scanf("%[^\n]%*c", path);
         if ((infile = fopen(path,"r")) == NULL)
@@ -21,11 +21,11 @@ int main ()
             printf("Error opening file!");
             exit(1);
         }
-      
+
         int count = 0;
         while(fread(&input[count], sizeof(struct record), 1, infile))
             count+=1;
-       
+
        fclose(infile);
 
        for(int i=0;i<count;i++)
@@ -42,7 +42,7 @@ int main ()
        }
        /*for(int i=0;i<count;i++)
        {
-           printf ("name = %s roll = %d marks = %d\n", input[i].name, 
+           printf ("name = %s roll = %d marks = %d\n", input[i].name,
                            input[i].roll, input[i].marks);
        }*/                                                                                      //To print data after sorting
 
@@ -53,10 +53,10 @@ int main ()
        {
            printf("Error opening file!");
            exit(1);
-       } 
+       }
 
        fwrite(&input, sizeof(struct record), count, outfile);
-      
+
        fclose(outfile);
       return 0;
-} 
+}
